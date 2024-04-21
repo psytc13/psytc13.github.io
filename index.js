3f4e6c95dec47f7ea2524c3fcf03176189d3cfab
psytc13 <167317677+psytc13@users.noreply.github.com>
2024-04-21 18:17:45 +0000

    changes

--- index.js
+++ index.js
@@ -1,18 +1,17 @@
-import { createClient } from
+ import { createClient } from
 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm';
 
 const supabase = createClient('https://rzeyacnujlqgcolnwrzn.supabase.co','eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJ6ZXlhY251amxxZ2NvbG53cnpuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTMzNTc1NzgsImV4cCI6MjAyODkzMzU3OH0.t1U-EPEVIAT7pgm0hukuPTrSqbT21FMaDX62QxuRg70');
 
-
+document.getElementById("peoplesubmit", peoplesubmit());
 
 async function peoplesubmit(){
-    //tempname = document.getElementById("name");
+    var tempname = document.getElementById("name").value;
+    /*const templic = document.getElementById("license");
     const { data, error } = await supabase
     .from('People')
     .select()
-    .eq('Name',"Rachel Smith");
-    console.log('Fetched data:', data);
-
+    .or('Name.ilike.%${tempname}%,LicenseNumber.eq.${templic}');*/
+    console.log('data: '+tempname);
 }
 
-peoplesubmit();
