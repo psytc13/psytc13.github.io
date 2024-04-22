0795671241ada3bb5ef108f7f54ae1a079eef849
psytc13 <167317677+psytc13@users.noreply.github.com>
2024-04-22 15:11:33 +0000

    changes

--- index.js
+++ index.js
@@ -3,15 +3,16 @@
 
 const supabase = createClient('https://rzeyacnujlqgcolnwrzn.supabase.co','eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJ6ZXlhY251amxxZ2NvbG53cnpuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTMzNTc1NzgsImV4cCI6MjAyODkzMzU3OH0.t1U-EPEVIAT7pgm0hukuPTrSqbT21FMaDX62QxuRg70');
 
-document.getElementById("peoplesubmit", peoplesubmit());
+const button = document.getElementById("peoplesubmit");
 
 async function peoplesubmit(){
-    var tempname = document.getElementById("name").value;
-    /*const templic = document.getElementById("license");
+    const tempname = document.getElementById("name").value;
+    const templic = document.getElementById("license").value;
     const { data, error } = await supabase
     .from('People')
     .select()
-    .or('Name.ilike.%${tempname}%,LicenseNumber.eq.${templic}');*/
-    console.log('data: '+tempname);
+    .ilike('Name','%'+tempname+'%'); 
+    console.log(data);
 }
 
+button.addEventListener("click", peoplesubmit);
